6907d072865342103b0c6b5ed63b460836964064
Shavez ahmed <ahmedshavez55@gmail.com>
2024-08-26 20:10:04 +0530

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Dynamic-Portfolio-/",
   plugins: [react()],
-  base:"/vite-deploy"
 })
